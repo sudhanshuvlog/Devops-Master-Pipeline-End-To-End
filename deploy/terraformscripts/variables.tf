be6def46bebacea7e56eab3cd41fe3830d773dca
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-08-15 11:50:18 +0530

    updated sg

--- deploy/terraformscripts/variables.tf
+++ deploy/terraformscripts/variables.tf
@@ -16,3 +16,22 @@ variable "instanceTagName"{
 variable "sg_name"{
     default = "WebserverSg"
 }
+
+variable "allowedPort"{
+    type = list 
+    default     = [
+    6443,  # Kubernetes API server
+    2379,  # etcd server client API
+    2380,  # etcd server peer API
+    10250, # Kubelet API
+    10251, # kube-scheduler
+    10252, # kube-controller-manager
+    30000, # NodePort Services start
+    32767,  # NodePort Services end
+    80,
+    22,
+    8080,
+    3000, 
+    9090
+  ]
+}
