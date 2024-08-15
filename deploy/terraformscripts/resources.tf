7e6ee4931ec50341d73f58c460a0e6e5cea23d0c
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-08-15 11:32:07 +0530

    fixed the bugs in terraform scripts

--- deploy/terraformscripts/resources.tf
+++ deploy/terraformscripts/resources.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "web" {
-  depends_on = [aws_key_pair.my_key_pair, aws_security_group.webserver_sg ]
-  ami   = data.aws_ami.latest_amazon_linux.id
+  depends_on = [aws_key_pair.my_key_pair, aws_security_group.webserver_sg]
+  ami = data.aws_ami.latest_amazon_linux.id
   instance_type = var.instanceType
   tags = {
     Name = "${var.instanceTagName}"
   }
   key_name = aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
-  count =2
+  count = 2
   provisioner "local-exec" {
-    command = "echo 'resource exectued succesfully'"
+    command = "echo 'resource executed successfully'"
   }
 }
 
@@ -24,48 +24,44 @@ resource "aws_security_group" "webserver_sg" {
   vpc_id      = data.aws_vpcs.default_vpc.ids[0]
 
   dynamic "ingress" {
-    for_each = [80,22,8080,3000,9090]
-    content { 
-    description      = "---"
-    from_port        = ingress.value
-    to_port          = ingress.value
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    for_each = [80, 22, 8080, 3000, 9090]
+    content {
+      description = "---"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "null_resource" "configureAnsibleInventory" {
-triggers ={
-  mytrigger = timestamp()
-}
-provisioner "local-exec" {
-    command = "echo [prod] > inventory"
+  triggers = {
+    mytrigger = timestamp()
   }
-
-}
-resource "null_resource" "configureansibleinventoryIPdetails"{
-triggers ={
-  mytrigger = timestamp()
-}
-provisioner "local-exec" {
+  provisioner "local-exec" {
     command = <<-EOT
+      echo "[k8s-master]" > inventory
+      echo "${aws_instance.web.0.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=mykey" >> inventory
+      echo "[k8s-workers]" >> inventory
       for ip in ${join(" ", aws_instance.web[*].public_ip)}; do
-        echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=mykey" >> inventory
+        if [ "$ip" != "${aws_instance.web.0.public_ip}" ]; then
+          echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=mykey" >> inventory
+        fi
       done
     EOT
-    }
+  }
 }
 
-resource "null_resource" "destroy_resource"{
+resource "null_resource" "destroy_resource" {
   provisioner "local-exec" {
-    when = destroy
+    when    = destroy
     command = "echo destroying resources.. > gfgdestroy.txt"
   }
 }
