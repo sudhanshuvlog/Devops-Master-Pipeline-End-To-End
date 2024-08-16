4ca9001dd622bbfcbdeee5c086d7d88efa790ad2
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-08-17 00:16:49 +0530

    new updates

--- deploy/terraformscripts/resources.tf
+++ deploy/terraformscripts/resources.tf
@@ -33,6 +33,12 @@ resource "aws_security_group" "webserver_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
